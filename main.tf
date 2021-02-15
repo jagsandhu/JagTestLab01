9592c96e27c79e2fd0f2a7494415b9514e90f519
jagsandhu <55237443+jagsandhu@users.noreply.github.com>
2021-02-15 16:22:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = ""
+            version = "2.47.0"
             }
     }
 }
